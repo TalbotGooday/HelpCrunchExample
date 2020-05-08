e30becd74424d5253e3087dd79f05ad99c82ad5d
gooday <alexey.mostovoy.w@gmail.com>
2020-05-08 10:39:30 +0300

    Initial commit

--- app/src/main/java/com/gapps/example/App.kt
+++ app/src/main/java/com/gapps/example/App.kt
@@ -2,8 +2,6 @@ package com.gapps.example
 
 import androidx.multidex.MultiDexApplication
 import com.helpcrunch.library.core.HelpCrunch
-import com.helpcrunch.library.options.HCOptions
-import com.helpcrunch.library.options.design.HCTheme
 
 class App : MultiDexApplication() {
 	override fun onCreate() {
@@ -12,9 +10,7 @@ class App : MultiDexApplication() {
 		HelpCrunch.initialize(
 				"ledgers",
 				3,
-				"a4iBedJfx/VPHn4Hrq9DhzYP0jlFvToTPKZMYnJpqujDl/c4g7qYOMHmryAthGVW2skrCr6qZpaU40X0FX1Lsg==",
-				null,
-				HCOptions.Builder().setTheme(HCTheme.Builder(R.color.colorPrimaryChat).build()).build()
+				"a4iBedJfx/VPHn4Hrq9DhzYP0jlFvToTPKZMYnJpqujDl/c4g7qYOMHmryAthGVW2skrCr6qZpaU40X0FX1Lsg=="
 		)
 	}
 }
--- app/src/main/java/com/gapps/example/MainActivity.kt
+++ app/src/main/java/com/gapps/example/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.gapps.example
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.helpcrunch.library.core.Callback
 import com.helpcrunch.library.core.HelpCrunch
-import com.helpcrunch.library.options.HCOptions
-import com.helpcrunch.library.options.design.HCTheme
+import com.helpcrunch.library.core.options.HCOptions
+import com.helpcrunch.library.core.options.design.HCTheme
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +15,21 @@ class MainActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_main)
 
 		open_chat.setOnClickListener { openChat() }
+
+		logout.setOnClickListener { logout() }
 	}
 
 	private fun openChat() {
-		HelpCrunch.showChatScreen(this)
+		HelpCrunch.showChatScreen(
+				HCOptions.Builder().setTheme(HCTheme.Builder(R.color.colorPrimary, true).build()).build()
+		)
+	}
+
+	private fun logout() {
+		HelpCrunch.logout(object : Callback<Any?>() {
+			override fun onSuccess(result: Any?) {
+				Toast.makeText(this@MainActivity, "Logged out", Toast.LENGTH_SHORT).show()
+			}
+		})
 	}
 }
